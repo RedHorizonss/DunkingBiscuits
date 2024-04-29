8feb44288c691426fb037e841a61a3ebe9e7372b
Fay Abu-Al-Timen <fay.a.timen@gmail.com>
2024-04-29 23:33:22 +0100

    making plots and cleaning up a few functions also deleted extra datasets that are not used.

--- codey_functions.py
+++ codey_functions.py
@@ -15,30 +15,18 @@ sns.set_theme(style="whitegrid", font_scale=1.25 ,rc={"xtick.bottom" : True, "yt
                                     "axes.spines.right" : False, "axes.spines.top" : False,
                                     'axes.linewidth': 2, 'axes.edgecolor':'black'})
 
-palette = ["#21918c", "#addc30", "#472d7b", "#5ec962", "#3b528b"]
+palette = ["#21918c", "#addc30", "#472d7b", "#5ec962", "#3b528b", "#28ae80"]
 sns.set_palette(palette)
 
 class biscuit_models():
-    def __init__(self, df, washburn_eqn=False):
+    def __init__(self, df):
         self.df = df
-        
-        if washburn_eqn:
-            self.washburn_eqn()
             
     def define_features(self, feature_columns, target_column):
         self.feature_columns = feature_columns
         self.target_column = target_column
         self.X = self.df[self.feature_columns]
         self.y = self.df[self.target_column]
-        
-    def washburn_eqn(self):
-        self.df['L_squared'] = self.df['L']**2
-        self.df['cos(phi)'] = np.cos(self.df['phi'])
-        self.df['2eta'] = 2*self.df['eta']
-        self.df['1/gamma'] = 1/self.df['gamma']
-        self.df['1/t'] = 1/self.df['t']
-        self.df['1/cos(phi)'] = 1/self.df['cos(phi)']
-        self.df['washburn_eqn'] = self.df['L_squared'] * self.df['2eta'] * self.df['1/gamma'] *self.df['1/t']*self.df['1/cos(phi)']
         
     def polynommial_features(self, degree=2):
         poly = PolynomialFeatures(degree=degree)
@@ -71,7 +59,7 @@ class biscuit_models():
         print(f'Root Mean Squared Error: {np.sqrt(self.mse):.3e}')
         print(f'$R^{2}$: {self.r2:.3f}')
         
-    def plot_predictions(self, xlabel='Actual', ylabel='Predicted'):
+    def plot_predictions(self, xlabel='Actual', ylabel='Predicted', save_image=False, filename=None):
         plt.figure(figsize=(6, 5))
         plt.scatter(self.y_test, self.y_pred, label=f'$R^{2}$: {self.r2:.3f}', alpha=0.8)
         plt.plot([self.y_test.min(), self.y_test.max()], [self.y_test.min(), self.y_test.max()],
@@ -79,17 +67,21 @@ class biscuit_models():
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.legend()
+        if save_image:
+            plt.savefig(filename, dpi = 900)
         plt.show()
         
-    def plot_residuals(self):
+    def plot_residuals(self, save_image=False, filename=None):
         residuals = self.y_test - self.y_pred
         plt.figure(figsize=(6, 5))
         plt.scatter(self.y_pred, residuals, alpha=0.8)
         plt.axhline(y=0, color='black', lw=2, linestyle='--')
         plt.xlabel('Predicted Values')
         plt.ylabel('Prediction Residuals')
+        if save_image:
+            plt.savefig(filename, dpi = 900)
         
-    def plot_feature_importances(self):
+    def plot_feature_importances(self, save_image=False, filename=None):
         importances = self.model.feature_importances_
         indices = np.argsort(importances)[::-1]
         plt.figure(figsize=(6, 5))
@@ -97,8 +89,22 @@ class biscuit_models():
         plt.bar_label(container, labels=[f'{importances[i]:.3f}' for i in indices], label_type='center', color='white')
         plt.xticks(range(self.X_train.shape[1]), [self.feature_columns[i] for i in indices], rotation=90)
         plt.title('Feature Importances')
+        if save_image:
+            plt.savefig(filename, dpi = 900)
         plt.show()
 
+
+def washburn_eqn(df):
+    df['L_squared'] = df['L']**2
+    df['cos(phi)'] = np.cos(df['phi'])
+    df['1/cos(phi)'] = 1/df['cos(phi)']
+    
+    df['1/gamma'] = 1/df['gamma']
+    df['1/t'] = 1/df['t']
+    
+    df['washburn_eqn'] = df['L_squared'] * 2*df['eta'] * df['1/gamma'] *df['1/t']*df['1/cos(phi)']
+    
+    return df
 
 def scatter_plot_3D(x, y, z, color_by ,x_label, y_label, z_label, eye_x, eye_y, eye_z):
     fig = go.Figure(data=[go.Scatter3d(
